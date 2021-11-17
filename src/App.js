068257ed601197b89ba415e7a883d5b6d738a349
ja2z <93806968+ja2z@users.noreply.github.com>
2021-11-16 23:32:12 -0800

    wire in sigma data to chart

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { useMemo, useRef } from "react";
 import Highcharts from "highcharts";
 import HighchartsMore from "highcharts/highcharts-more";
 import HighchartsReact from "highcharts-react-official";
-import { client, useConfig, useElementData } from "@sigmacomputing/plugin";
+import {
+  client,
+  useConfig,
+  useElementData,
+  useElementColumns,
+} from "@sigmacomputing/plugin";
 //npm install highcharts highcharts-more highcharts-react-official
 
 client.config.configureEditorPanel([
   { name: "source", type: "element" },
-  { name: "dimension", type: "column", source: "source", allowMultiple: true },
+  { name: "dimension", type: "column", source: "source", allowMultiple: false },
   { name: "measures", type: "column", source: "source", allowMultiple: true },
 ]);
 
@@ -16,91 +21,89 @@ HighchartsMore(Highcharts);
 function App() {
   const config = useConfig();
   const sigmaData = useElementData(config.source);
+  const sigmaColumns = useElementColumns(config.source);
   const ref = useRef();
   const options = useMemo(() => {
     const dimensions = config.dimension;
     const measures = config.measures;
-console.log(dimensions,measures);
+
     // transform sigmaData --> treemap data
-    console.log(sigmaData);
-    const options = {
-      chart: {
-        polar: true,
-
-        type: 'line'
-      },
-      title: {
-        text: undefined,
-      },
-      pane: {
-        size: "80%",
-      },
-      xAxis: {
-        categories: [
-          "Sales",
-          "Marketing",
-          "Development",
-          "Customer Support",
-          "Information Technology",
-          "Administration",
-        ],
-        tickmarkPlacement: "on",
-        lineWidth: 0,
-      },
-
-      yAxis: {
-        gridLineInterpolation: "polygon",
-        lineWidth: 0,
-        min: 0,
-      },
-
-      tooltip: {
-        shared: true,
-        pointFormat:
-          '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>',
-      },
-
-      legend: {
-        align: "right",
-        verticalAlign: "middle",
-        layout: "vertical",
-      },
-
-      series: [
-        {
-          name: "Allocated Budget",
-          data: [43000, 19000, 60000, 35000, 17000, 10000],
-          pointPlacement: "on",
+    if (sigmaData?.[dimensions[0]]) {
+      let categories = [];
+      let series = [];
+      let yAxis = [];
+      for (let i = 0; i < sigmaData[dimensions[0]].length; i++) {
+        categories[i] = sigmaData[dimensions[0]][i];
+      }
+      for (let i = 0; i < measures.length; i++) {
+        yAxis[i] = {};
+        yAxis[i].lineWidth = 3;
+        yAxis[i].angle = 90 * i;
+        series[i] = {};
+        series[i].name = sigmaColumns[measures[i]].name;
+        series[i].pointPlacement = "on";
+        series[i].data = [];
+        series[i].data = sigmaData[measures[i]];
+        series[i].yAxis = i;
+      }
+      console.log(series);
+      const options = {
+        chart: {
+          polar: true,
         },
-        {
-          name: "Actual Spending",
-          data: [50000, 39000, 42000, 31000, 26000, 14000],
-          pointPlacement: "on",
+        title: {
+          text: undefined,
         },
-      ],
-
-      responsive: {
-        rules: [
-          {
-            condition: {
-              maxWidth: 500,
-            },
-            chartOptions: {
-              legend: {
-                align: "center",
-                verticalAlign: "bottom",
-                layout: "horizontal",
+        pane: {
+          size: "80%",
+        },
+        xAxis: {
+          categories: categories,
+          tickmarkPlacement: "on",
+          lineWidth: 0,
+        },
+        yAxis: yAxis,
+        /*
+        yAxis: {
+          gridLineInterpolation: "polygon",
+          lineWidth: 0,
+          min: 0,
+        },
+        */
+        tooltip: {
+          shared: true,
+          pointFormat:
+            '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>',
+        },
+        legend: {
+          align: "right",
+          verticalAlign: "middle",
+          layout: "vertical",
+        },
+        series: series,
+        responsive: {
+          rules: [
+            {
+              condition: {
+                maxWidth: 500,
               },
-              pane: {
-                size: "70%",
+              chartOptions: {
+                legend: {
+                  align: "center",
+                  verticalAlign: "bottom",
+                  layout: "horizontal",
+                },
+                pane: {
+                  size: "70%",
+                },
               },
             },
-          },
-        ],
-      },
-    };
-    return options;
-  }, [config, sigmaData]);
+          ],
+        },
+      };
+      return options;
+    }
+  }, [config, sigmaData, sigmaColumns]);
 
   return (
     <div>
