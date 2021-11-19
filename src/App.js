96403ae0eec73c3007276f283a95da95bda74093
ja2z <93806968+ja2z@users.noreply.github.com>
2021-11-18 22:18:41 -0800

    formatting, options, colors, polygon fill

--- src/App.js
+++ src/App.js
@@ -14,9 +14,24 @@ client.config.configureEditorPanel([
   { name: "source", type: "element" },
   { name: "dimension", type: "column", source: "source", allowMultiple: false },
   { name: "measures", type: "column", source: "source", allowMultiple: true },
+  { name: "Separate Axes y/n?", type: "text", defaultValue: "N" },
+  { name: "Polygon Fill y/n?", type: "text", defaultValue: "N" },
 ]);
 
 HighchartsMore(Highcharts);
+const seriesColors = [
+  "#ED5E4F",
+  "#C1A836",
+  "#964F7D",
+  "#DD9684",
+  "#005C4A",
+  "#C9C7F5",
+  "#FDCFB6",
+  "#ACE0DF",
+];
+const axisColor = "#B2B2B2";
+const spiderColor = "#666666";
+const spiderColorCategories = "#E5E5E5";
 
 function App() {
   const config = useConfig();
@@ -26,50 +41,62 @@ function App() {
   const options = useMemo(() => {
     const dimensions = config.dimension;
     const measures = config.measures;
+    const separateY =
+      client.config.getKey("Separate Axes y/n?") === "Y" ? true : false;
 
     // transform sigmaData --> treemap data
-    if (sigmaData?.[dimensions[0]]) {
-      let categories = [];
+    if (sigmaData?.[dimensions]) {
       let series = [];
       let yAxis = [];
-      for (let i = 0; i < sigmaData[dimensions[0]].length; i++) {
-        categories[i] = sigmaData[dimensions[0]][i];
-      }
+      const categories = sigmaData[dimensions];
       for (let i = 0; i < measures.length; i++) {
         yAxis[i] = {};
         yAxis[i].lineWidth = 3;
-        yAxis[i].angle = 90 * i;
+        yAxis[i].tickAmount = 5;
+        yAxis[i].angle = 45 * i;
+        yAxis[i].gridLineInterpolation = "polygon";
+        yAxis[i].lineColor = axisColor;
+        yAxis[i].gridLineColor = spiderColor;
+
         series[i] = {};
         series[i].name = sigmaColumns[measures[i]].name;
         series[i].pointPlacement = "on";
         series[i].data = [];
+        if (client.config.getKey("Polygon Fill y/n?") === "Y") {
+          series[i].type = "polygon";
+          series[i].opacity = "0.3";
+        } else {
+          series[i].type = "line";
+          series[i].opacity = "1";
+        }
         series[i].data = sigmaData[measures[i]];
-        series[i].yAxis = i;
+        series[i].color = seriesColors[i];
+        series[i].lineWidth = 2;
+        // if axes are separate, add a y index to each series
+        if (separateY) series[i].yAxis = i;
       }
-      console.log(series);
+      // if using a shared axis, take the first yAxis config only
+      if (!separateY) yAxis = yAxis[0];
+
       const options = {
         chart: {
           polar: true,
+          height: (9 / 16) * 100 + "%",
+          backgroundColor: "transparent",
         },
         title: {
           text: undefined,
         },
         pane: {
-          size: "80%",
+          //size: "80%",
         },
         xAxis: {
           categories: categories,
           tickmarkPlacement: "on",
           lineWidth: 0,
+          gridLineColor: spiderColorCategories,
         },
         yAxis: yAxis,
-        /*
-        yAxis: {
-          gridLineInterpolation: "polygon",
-          lineWidth: 0,
-          min: 0,
-        },
-        */
         tooltip: {
           shared: true,
           pointFormat:
@@ -80,12 +107,17 @@ function App() {
           verticalAlign: "middle",
           layout: "vertical",
         },
+        plotOptions: {
+          series: {
+            lineWidth: 1,
+          },
+        },
         series: series,
         responsive: {
           rules: [
             {
               condition: {
-                maxWidth: 500,
+                maxWidth: 700,
               },
               chartOptions: {
                 legend: {
@@ -94,7 +126,7 @@ function App() {
                   layout: "horizontal",
                 },
                 pane: {
-                  size: "70%",
+                  //size: "70%",
                 },
               },
             },
